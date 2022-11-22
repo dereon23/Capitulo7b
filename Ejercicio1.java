adea1c31c43cefe3e26efdc29365460694d37063
dereon23 <115071403+dereon23@users.noreply.github.com>
2022-11-22 12:11:32 +0100

    Add files via upload

---
+++ Ejercicio1.java
@@ -0,0 +1,27 @@
+package Capitulo7b;
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+		
+		int[][] tabla = new int[3][6];
+		tabla[0][0]=0;
+		tabla[0][1]=30;
+		tabla[0][2]=2;
+		tabla[0][5]=5;
+		tabla[1][0]=75;
+		tabla[1][4]=0;
+		tabla[2][2]=-2;
+		tabla[2][3]=9;
+		tabla[2][5]=11;
+
+		for(int i=0;i<3;i++) {
+			for(int j=0;j<6;j++) {
+				System.out.printf("%4d",tabla[i][j]);
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
